eda16a694b1978323bdbcd1987e945479e7eee14
candacedo <candace.do@gmail.com>
2020-03-14 23:26:05 -0700

    created Component class

---
+++ components/Component.js
@@ -0,0 +1,25 @@
+"use strict"
+
+class Component {
+    
+    constructor() {}
+
+    setMass(mass) {
+        this.mass = mass;
+    }
+
+    getMass() {
+        return this.mass;
+    }
+
+    getNormal() {}
+
+    getDrag() {}
+
+    getLift() {}
+
+    getCP() {}
+
+    getCG() {}
+
+}
