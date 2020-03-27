54aa5f79941a2fe9039932f4addfa0958b9356ea
candacedo <candace.do@gmail.com>
2020-03-26 23:46:06 -0700

    added toggle for filled transition

--- components/Transition.js
+++ components/Transition.js
@@ -11,6 +11,7 @@ class Transition extends AxialComponent {
         this.state.endRadius = endRadius;
         this.state.length = length;
         this.state.thickness = thickness;
+        this.state.filled = false;
     }
 
     _calcPoints() {
@@ -25,7 +26,7 @@ class Transition extends AxialComponent {
         let r = this.state.startRadius - x;
         let h = this.state.length - 2 * this.state.thickness;
         let massInner = (R**2 + R * r + r**2) * 1/3 * h * Math.PI * this.state.density;
-        this.mass = this._calcVolume() * this.state.density - massInner;
+        this.mass = this.state.filled? this._calcVolume() * this.state.density : this._calcVolume() * this.state.density - massInner;
 
         return this.mass;
     }
@@ -43,13 +44,17 @@ class Transition extends AxialComponent {
         let comInner = h * (R**2 + 2 * R * r + 3 * r**2) / (4 * (R**2 + R * r + r**2));
         let massInner = (R**2 + R * r + r**2) * 1/3 * h * Math.PI * this.state.density;
         
-        this.cg = (this._calcVolume() * this.state.density * comOuter - massInner * comInner) / (this._calcVolume() * this.state.density - massInner);
+        this.cg = this.state.filled ? comOuter : (this._calcVolume() * this.state.density * comOuter - massInner * comInner) / (this._calcVolume() * this.state.density - massInner);
 
         return this.cg;
     }   
+
+    toggleFilled() {
+        super.setState({filled: !this.state.filled});
+    }
 }
 
-let bt = new Transition(2.5, 2.3, 20, 0.68, 1.2, 0, 10, 10, 0, 0);
+let bt = new Transition(2.5, 2.3, 7.5, 0.68, 1.2, 0, 10, 10, 0, 0);
 bt.setState({});
 console.log(bt.mass);
 console.log(bt.cg);
