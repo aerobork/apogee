306bdf996601f2772408a6b3e2d9f476a2502635
candacedo <candace.do@gmail.com>
2020-04-03 20:29:13 -0700

    added intersection function

--- utils.js
+++ utils.js
@@ -1,1 +1,25 @@
+"use strict"
 
+
+function finIntersect(x, points, coord) {
+    let topEdge = [];
+    let bottomEdge = [];
+
+    this.points.map((point, idx) => {
+        if (x < points[idx][coord] && x > points[idx + 1][coord]) {
+            bottomEdge = [points[idx], points[idx + 1]];
+        }
+        if (x > points[idx][coord] && x < points[idx + 1][coord]){
+            topEdge = [points[idx], points[idx + 1]];
+        }
+    })
+
+    return [topEdge, bottomEdge];
+}
+
+function intersect(x, points) {
+    let m = (points[0][1] - points[1][1]) / (points[0][0] - points[1][0]);
+    let b = points[0][1] - m * points[0][0];
+
+    return m * x + b;
+}
