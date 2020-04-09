6072e4ecb2ddfaff06320e78ce8baf8645803b12
candacedo <candace.do@gmail.com>
2020-04-08 21:35:15 -0700

    finished fins

--- components/FinSet.js
+++ components/FinSet.js
@@ -45,12 +45,15 @@ class FinSet {
             case "elliptical":
                 let a = this.state.height;
                 let b = this.state.rootChord / 2;
-                for (let y = 0; y < this.state.length; y += 0.1) {
-                    x = ((1 - (y - b)**2 / b**2) / a**2)**0.5; 
-                    points.push([x,y]);
+                for (let y = 0; y < this.state.rootChord; y += 0.2) {
+                    let x = ((1 - (y - b)**2 / b**2) / a**2)**0.5;
+
+                    points.push([x,-1 * y]);
                 }
+                points.push([0, -1 * this.state.rootChord]);
+                console.log(points);
                 break;
-            case "free-form": 
+            case "freeform": 
                 points = this.freeFormPoints;
                 break;
         }
@@ -65,6 +68,8 @@ class FinSet {
         }
 
         this._calcPoints();
+        this._calcArea();
+        this._calcCP();
 
     }
 
@@ -74,7 +79,8 @@ class FinSet {
     }
 
     _calcMass() {
-
+        this.mass = this.area * this.state.density;
+        return this.mass;
     }
 
     setCG(cg) {
@@ -82,13 +88,46 @@ class FinSet {
         this.overrideCG = true;
     }
 
-    _calcCG() {
+    _calcCG() { // TODO: check this entire thing 
+        let x = 0;
+        let mass = 0;
+        let com = 0;
 
+        // loop
+        while (true){
+            let edges = utils.finIntersect(x, this.points, 0);
+            if (edges[0].length == 0 || edges[1].length == 0) {
+                break;
+            }
+
+            let b1 = edges[0][0][1] - edges[1][1][1]; 
+            let tx = edges[0][1][0];
+            let bx = edges[1][0][0];
+
+            
+            let divX = Math.min(tx, bx);
+            let h = divX - x;
+
+            let b2 = utils.intersect(divX, edges[0]) - utils.intersect(divX, edges[1]);
+            
+            com += (h * (bx**2 + 2 * bx * tx + 3 * tx**2) / (4 * (bx**2 + bx * tx + tx**2)));
+            mass += (b1 + b2) * h * 0.5 * this.state.density;
+
+            x = divX;
+
+            if (utils.equalTo(b2, 0)){
+                break;
+            }
+        }
+
+        this.cg = com / mass;
+        return this.cg;
     }
 
     _calcCP() { 
         let x = 0;
-        let area = 0;
+        let xmac = 0;
+        let maclength = 0;
 
         // loop
         while (true){
@@ -98,20 +137,40 @@ class FinSet {
                 break;
             }
 
-            let b1 = edges[0][0][1] - edges[1][1][1]; 
+            let b1 = utils.intersect(x, edges[0]) - utils.intersect(x, edges[1]); 
             let tx = edges[0][1][0];
             let bx = edges[1][0][0];
             
-            let divX = Math.min([tx, bx]);
+            let divX = Math.min(tx, bx);
             let h = divX - x;
 
             let b2 = utils.intersect(divX, edges[0]) - utils.intersect(divX, edges[1]);
+            
+            console.log(edges);
+            console.log("b1: " + b1);
+            console.log("b2: " + b2);
+
+            let y = utils.intersect(x, edges[0]);
+            let m1 = (edges[0][0][1] - edges[0][1][1]) / (edges[0][0][0] - edges[0][1][0]);
+            let m2 = (edges[1][1][1] - edges[1][0][1]) / (edges[1][1][0] - edges[1][0][0]);
+            let l = b1;
+            console.log("m1: " + m1);
+            console.log("m2: " + m2);
+
+            let xmacB = (m1 - m2) * y / 2 * h**2 + m1 * l * h**2 / 2 + (m1 - m2) * m1 * h**3 / 3 + y * l * h;
+            //let xmacA = (m1 - m2) * y / 2 * x**2 + m1 * l * x**2 / 2 + (m1 - m2) * m1 * x**3 / 3 + y * l * x;
+            xmac += xmacB;
+            console.log("xmac: " + xmac);
+
+            let maclengthB = l**2 * h + l * (m1 - m2) * h**2 + (m1 - m2)**2 * h**3 / 3;
+            //let maclengthA = l**2 * x + l * (m1 - m2) * x**2 + (m1 - m2)**2 * x**3 / 3;
+            maclength += maclengthB;
+            console.log("mac length: " + maclength);
 
             // b1, b2, h
             // int(Xle * C dy);
             //Xle = y + m * x
             //C = length + (m1 + m2) * x
-
 
             /*integral 0->s(
                 y * l + 
@@ -119,15 +178,20 @@ class FinSet {
                 m1 * x * l +
                 (m1 + m2) * m1 * x^2, dx
             )*/
-
-
             x = divX;
 
             if (utils.equalTo(b2, 0)){
                 break;
             }
         }
-    }
+        xmac /= this.area;
+        maclength /= this.area;
+
+        console.log("xmac: " + xmac);
+        console.log("mac length: " + maclength);
+        this.cp = xmac - maclength / 4;
+        return this.cp;
+    
     }
 
     _calcArea() {
@@ -137,7 +201,6 @@ class FinSet {
         // loop
         while (true){
             let edges = utils.finIntersect(x, this.points, 0);
-
             if (edges[0].length == 0 || edges[1].length == 0) {
                 break;
             }
@@ -145,8 +208,9 @@ class FinSet {
             let b1 = edges[0][0][1] - edges[1][1][1]; 
             let tx = edges[0][1][0];
             let bx = edges[1][0][0];
+
             
-            let divX = Math.min([tx, bx]);
+            let divX = Math.min(tx, bx);
             let h = divX - x;
 
             let b2 = utils.intersect(divX, edges[0]) - utils.intersect(divX, edges[1]);
@@ -158,6 +222,19 @@ class FinSet {
                 break;
             }
         }
+
+        this.area = area;
+        return this.area;
     }
 
 }
+
+//let fin = new FinSet("freeform", 3, 0, 0, 5, 5, 5, 5, 5, 5, 5, [[0, 0], [5, -2.5], [5, -7.5], [0, -5]], 0.68, .3, 0, 0, 0, 0, 0);
+//let fin = new FinSet("freeform", 3, 0, 0, 5, 5, 5, 5, 5, 5, 5, [[0, 0], [5, -2.5], [7.5, -5], [5, -7.5], [0, -5]], 0.68, .3, 0, 0, 0, 0, 0);
+//let fin = new FinSet("freeform", 3, 0, 0, 5, 5, 5, 5, 5, 5, 5, [[0, 0], [2.5, -2.5], [0,-5]], 0.68, .3, 0, 0, 0, 0, 0);
+let fin = new FinSet("elliptical", 3, 0, 0, 5, 5, 5, 5, 0, 0, 0, [], 0.68, 0.3, 0, 0, 0, 0, 0);
+
+//fin._setState({});
+console.log("points: " + fin.points);
+console.log("area: " + fin.area);
+console.log("cp: " + fin.cp);
