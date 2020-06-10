ae4fd7ed3e2435fb9f3594fb07b51ace86f6ee2e
candacedo <candace.do@gmail.com>
2020-06-09 22:27:53 -0700

    WhowowoOOOoooooOOO?????? -candace

---
+++ Imports.js
@@ -0,0 +1,28 @@
+const AxialOuterComponent = require('./components/AxialOuterComponent');
+const BodyTube = require("./components/BodyTube.js");
+const Bulkhead = require('./components/Bulkhead.js');
+const CenteringRing = require('./components/CenteringRing.js');
+const Component = require('./components/Component.js');
+const ComponentSeries = require('./components/ComponentSeries.js');
+const Coupler  = require('./components/Coupler.js');
+const EngineBlock = require('./components/EngineBlock.js');
+const FinSet = require("./components/FinSet.js");
+const InnerComponent = require("./components/InnerComponent.js");
+const InnerTube = require('./components/InnerTube.js');
+const MassComponent = require('./components/MassComponent.js');
+const Motor =  require('./components/Motor.js');
+const Nosecone = require('./components/Nosecone.js');
+const OuterComponent = require('./components/OuterComponent.js');
+const Parachute = require('./components/Parachute.js');
+const Shockcord = require('./components/Shockcord.js');
+const Streamer  = require('./components/Streamer.js');
+const Transition = require('./components/Transition.js');
+
+const Rocket = require('./Rocket.js');
+const Simulation =  require('./Simulation.js');
+
+module.exports = {AxialOuterComponent, BodyTube, Bulkhead, CenteringRing, Component, ComponentSeries, Coupler, EngineBlock, FinSet, InnerComponent, InnerTube, MassComponent,
+                  Motor, Nosecone, OuterComponent, Parachute, Shockcord, Streamer, Transition, Rocket, Simulation};
+
+
+
